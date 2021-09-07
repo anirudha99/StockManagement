40e86bd40052f2baad77ec8592b758a155166490
anirudha mayya <anirudha.mayya@gmail.com>
2021-09-07 20:55:32 +0530

    Welcome message

---
+++ src/com/stockmanage/StockMain.java
@@ -0,0 +1,14 @@
+package com.stockmanage;
+
+import java.util.Scanner;
+
+import org.json.simple.JSONArray;
+
+public class StockMain {
+	public static Scanner sc = new Scanner(System.in);
+	public static JSONArray stockList = new JSONArray();
+	public static void main(String[] args) {
+		System.out.println("------- Welcome to Stock Management -------");
+	}
+
+}
